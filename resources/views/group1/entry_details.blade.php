01eb10ec0be13337990c3d89c57e8d80b9412498
Stephen Fritsen <sfritsen@gmail.com>
2018-10-03 05:56:17 +0000

    Refactored stuff

--- resources/views/group1/entry_details.blade.php
+++ resources/views/group1/entry_details.blade.php
@@ -3,40 +3,40 @@
         <th colspan="4">Employee Information</th>
     </tr>
     <tr>
-        <td class="section_label">Name</td>
+        <td class="label">Name</td>
         <td>{{ $record->emp_info_name }}</td>
-        <td class="section_label">City</td>
+        <td class="label">City</td>
         <td>{{ $record->emp_info_city }}</td>
     </tr>
     <tr>
-        <td class="section_label">Agent ID</td>
+        <td class="label">Agent ID</td>
         <td>{{ $record->emp_info_id }}</td>
-        <td class="section_label">Manager</td>
+        <td class="label">Manager</td>
         <td>{{ $record->emp_info_mgr_name }}</td>
     </tr>
     <tr>
         <th colspan="4">Record Details</th>
     </tr>
     <tr>
-        <td class="section_label">Phone Number</td>
+        <td class="label">Phone Number</td>
         <td>{{ $record->phone_number }}</td>
-        <td class="section_label">Lynx Ticket</td>
+        <td class="label">Lynx Ticket</td>
         <td>{{ $record->lynx }}</td>
     </tr>
     <tr>
-        <td class="section_label">Chat ID</td>
+        <td class="label">Chat ID</td>
         <td>{{ $record->chat_session_id }}</td>
-        <td class="section_label">Incident Type</td>
+        <td class="label">Incident Type</td>
         <td>{{ $record->dd_incident_type }}</td>
     </tr>
     <tr>
-        <td class="section_label">Equipment</td>
+        <td class="label">Equipment</td>
         <td>{{ $record->dd_equip_type }}</td>
-        <td class="section_label">Troubleshooting</td>
+        <td class="label">Troubleshooting</td>
         <td>{{ $record->dd_troubleshooting }}</td>
     </tr>
     <tr>
-        <td class="section_label">Client Troubleshooting</td>
+        <td class="label">Client Troubleshooting</td>
         <td>
             @if ($record->client_no_ts === 1)
                 Yes
@@ -44,7 +44,7 @@
                 No
             @endif
         </td>
-        <td class="section_label">Invalid Insight</td>
+        <td class="label">Invalid Insight</td>
         <td>
             @if ($record->invalid_ref === 1)
                 Yes
@@ -54,9 +54,9 @@
         </td>
     </tr>
     <tr>
-        <td class="section_label">Resolution</td>
+        <td class="label">Resolution</td>
         <td>{{ $record->dd_resolution }}</td>
-        <td class="section_label"></td>
+        <td class="label"></td>
         <td></td>
     </tr>
     <tr>
--- resources/views/partials/category_boxes.blade.php
+++ resources/views/partials/category_boxes.blade.php
@@ -9,9 +9,9 @@
     <div class="container-fluid nopadding">
         <div class="row submenu">
             <div class="col">
-                <a href="{{ url('g1_cat_boxes/1/0') }}"><div class="item">Home</div></a>
+                <a href="{{ url('g1_cat_boxes/1/0') }}"><div class="item">Level 1 Main</div></a>
                 <a href=""><div class="item">Trash Bin</div></a>
-                <a href=""><div class="item">Another Item</div></a>
+                <a href="" data-toggle="modal" data-target="#helpModal"><div class="item">Help</div></a>
             </div>
         </div>
         <div class="row">
@@ -19,22 +19,22 @@
                 To add a new item, navigate to the section you want to add the item too and enter the value in the field below.
             </div>
         </div>
-        <div class="row">
+        <div class="row d-flex align-items-center">
             <div class="col">
-                <div class="form-group">
-                    <input type="text" id="new_item" class="form-control" placeholder="Add New Item" value="{{ old('new_item') }}" aria-describedby="newHelpBlock" autofocus>
-                    <input type="hidden" id="type" value="{{ $type }}">
-                    <input type="hidden" id="is_under" value="{{ $is_under }}">
-                    <small>Tip! After entering a value, press TAB then ENTER</small>
-                </div>
+                <input type="text" id="new_item" class="form-control" placeholder="Add New Item" value="{{ old('new_item') }}" aria-describedby="newHelpBlock" autofocus>
+                <input type="hidden" id="type" value="{{ $type }}">
+                <input type="hidden" id="is_under" value="{{ $is_under }}">
             </div>
             <div class="col-auto">
                 <button type="button" id="add_item_btn" class="btn form_btn">Submit</button>
             </div>
             <div class="col">
-                <div id="testresults"></div> {{-- DEBUGGING RETURN INFO ONLY --}}
+                <div id="output_message"></div>
             </div>
         </div>
+
+        <hr>
+
         <div class="row">
             <div class="col">
                 {!! $nav_label !!}
@@ -104,6 +104,65 @@
         </div>
     </div>
 
+    {{-- Help Modal --}}
+    <div class="modal fade" id="helpModal" tabindex="-1" role="dialog" aria-labelledby="helpModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content custom_modal">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="helpModalLabel">Category Box Help</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="section_title">Add New Item</div>
+                    <div class="section_info">
+                        <ol>
+                            <li>In the table below, navigate to the section you want to add an item too.</li>
+                            <li>Enter the text in the "Add New Item" field worded as you want.</li>
+                            <li>Press Submit</li>
+                        </ol>
+                        The item should appear in list sorted automatically and active.
+                    </div>
+
+                    <div class="section_title">Editing</div>
+                    <div class="section_info">
+                        At this time, editing is not permitted.  Entries are logged with the items value and have a general subject to them.  By editing, you run the risk of altering 
+                        the subject which can change the previous records accuracy.
+                        <p>
+                            If you want an item changed, there are 2 options.
+                            <ol>
+                                <li>Delete the item then create a new one with the changes you want</li>
+                                <li>Email an admin which can alter the item</li>
+                            </ol>
+                        </p>
+                    </div>
+
+                    <div class="section_title">Disabling / Removing</div>
+                    <div class="section_info">
+                        The toggle on the right of each element controls its state.
+                        <ul>
+                            <li>When the toggle is to the right and showing green, the item is active and available for use.</li>
+                            <li>Toggle to the left and grey, the item is disabled and not shown in the live menu.</li>
+                        </ul>
+                    </div>
+
+                    <div class="section_title">Deleting items</div>
+                    <div class="section_info">
+                        <p>
+                            By pressing the "Delete" button, it will remove the item from the list in both active and disabled states.  This will not actually delete the item 
+                            but instead set a flag on it marking it as deleted.
+                        </p>
+                        <p>To recover a deleted item, click "Trash Bin" from the top submenu and click "Restore".  This will move the item back into your main list.</p>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn modal_btn" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script>
     $(document).ready(function(){
 
@@ -113,6 +172,13 @@
             var item = $("#new_item").val();
             var type = $("#type").val();
             var is_under = $("#is_under").val();
+
+            // Validates the item string for length
+            var validate = item.length;
+            if (validate > '40') {
+                $("#output_message").hide().html('<span class="error_msg">Whoa, '+validate+' characters!  Limit 40 or less.</span>').fadeIn(400).delay(2000).fadeOut(400);
+                throw "Error";
+            }
 
             // Sends the info to the route to be saved
             $.ajax({
@@ -142,7 +208,7 @@
                         '</td></tr>').fadeIn(600);
 
                     // Display message
-                    $("#testresults").html(data.item);
+                    $("#output_message").hide().html(data.item).fadeIn(400).delay(2000).fadeOut(400);
 
                     // Sort rows based on label
                     var asc = 'asc';
@@ -174,7 +240,7 @@
                 type: "get",
                 url: "{{ url('g1_cat_boxes_edit') }}/"+id+"/"+state,
                 success: function(data){
-                    $("#testresults").html(id + ' = ' + state); /* DEBUGGING or success message */
+                    // $("#output_message").hide().html(id + ' = ' + state).fadeIn(400).delay(2000).fadeOut(400); /* DEBUGGING or success message */
                     $("#updated_val_"+id).html(data); /* Sends the new update time to the field */
                 }
             });
@@ -196,7 +262,7 @@
                 type: "get",
                 url: "{{ url('g1_cat_boxes_delete') }}/"+id,
                 success: function(data){
-                    $("#testresults").html(id + ' deleted'); /* DEBUGGING or success message */
+                    // $("#output_message").hide().html(id + ' deleted').fadeIn(400).delay(2000).fadeOut(400); /* DEBUGGING or success message */
                     $("#updated_val_"+id).html(data); /* Sends the new update time to the field */
                 }
             });
